bbe417e67d22ccb55c8b41851fcec277fdf30533
RIITKAGARWAL <agarwalritik895@gmail.com>
2024-07-14 04:54:57 +0530

    speech recognition

---
+++ index.js
@@ -0,0 +1,10 @@
+function voice(){
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang= "en-GB"
+    recognition.onresult = function (event){
+        console.log(event);
+        document.getElementById("speechToText").value=
+        event.results[0][0].transcript;
+    }
+    recognition.start();
+}
